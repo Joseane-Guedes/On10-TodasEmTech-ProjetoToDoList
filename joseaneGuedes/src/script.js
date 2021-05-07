4892bcfb9c4555859ca00a11d3a43620b744f555
Joseane-Guedes <joseane_23@hotmail.com>
2021-05-06 23:17:22 -0300

    projeto final

--- joseaneGuedes/src/script.js
+++ joseaneGuedes/src/script.js
@@ -1,80 +1,63 @@
-/*clicar e add uma tarefa embaixo> limpar o campo da tarefa> clicar no botao e excluir a tarefa>*/
-/*pegar input criar um p e colocar o ele dentro> pegar o p e colocar dentro de uma div>
-/*deletar a tarefa quando  clicar o botão de excluir */
+// Adotando querySelector por ser uma boa prática de programação. 
+/* Document.querySelector() ===> Retorna o primeiro elemento dentro do documento (usando ordenação em profundidade, pré-ordenada e transversal dos nós do documento) que corresponde ao grupo especificado de seletores. Fonte:  https://developer.mozilla.org/pt-BR/docs/Learn/Getting_started_with_the_web/JavaScript_basics e cursoemvideo - Gustavo Guanabara */
 
+const input = document.querySelector('#todoInput'); // nó do input
+const botao = document.querySelector('#todoSubmit'); //nó da botao
+const listaTarefas = document.querySelector('#todoLista'); //nó da ul
+const formulario = document.querySelector('#todoForm'); //nó do formulário
+const botaoLimpar = document.querySelector('#todoRemoverTodos'); //nó do botão limpar 
+const botaoMarcarTodas = document.querySelector('#todoMarcarTodos'); // nó do botão marcar todos
 
-// nó do input
-const inputado = document.querySelector('#todoInput');
-
-//nó da botao
-const botao = document.querySelector('#todoSubmit');
-
-//nó da ul
-const listaTarefas = document.querySelector('#todoLista');
-
-//nó do formulário
-const formulario = document.querySelector('#todoForm');
-
-//nó do botão limpar 
-const botaoLimpar = document.querySelector('#todoRemoverTodos');
-
-// nó do botão marcar todos
-const botaoMarcarTodas = document.querySelector('#todoMarcarTodos');
-
-// criar evento botão
+// criar evento botão para adicionar elemento
 botao.addEventListener('click', (event) => {
-    //previne que o formulário seja enviado ao clicar, espera processar o que precisa da função
-    event.preventDefault();
-
+    event.preventDefault(); /*previne que o formulário seja enviado ao clicar, espera processar o que precisa da função */
     const elementoLista = document.createElement('li');
     const textoElemento = document.createElement('p');
+    textoElemento.classList.add("elementoLista");
     const deletaElemento = document.createElement('span');
 
     // atribuímos o valor do input ao <p> criado a partir do clique no botão
-    textoElemento.innerText = inputado.value;
-    deletaElemento.innerText = 'X';
+    textoElemento.innerText = input.value;
+    deletaElemento.innerText = '🗑️';
     // para excluir itens da lista, precisamos criar um elemento que represente isso, e colocar evento para remover o nó do dom
 
+    //verificação: input inserido é vazio ou composto apenas por espaços:
     if (textoElemento.innerText.trim() === '') {
-        alert("Insira uma tarefa");
+        document.querySelector('#alertaErro').innerHTML = 'Não foi possível atualizar: Por favor, digite  sua tarefa!'
     } else {
         //pegamos o nó mãe , e acrescentamos o elemento filho com o append
         listaTarefas.appendChild(elementoLista);
         elementoLista.appendChild(textoElemento);
         elementoLista.appendChild(deletaElemento);
-
+        document.querySelector('#alertaErro').innerHTML = '';
         // pegamos o nó do formulário e utilizamos um método para limpar o input
         formulario.reset();
-
     }
 
-    //função de checar
+    ////função que checa que adiciona uma classe a cada p de cada li e, a partir disso, altera o estilo do p
     textoElemento.addEventListener("click", () => {
         textoElemento.classList.add("checked");
     });
 
-    //função de deletar
+    //função de deletar elemento a li desejada
     deletaElemento.addEventListener("click", () => {
         listaTarefas.removeChild(elementoLista);
         // outra de forma de resolver >>> elementoLista.remove();
     });
 
 
-    // //TODO função de marcar todos 
+    // Função de marcar todo p 
     botaoMarcarTodas.addEventListener('click', () => {
-        // o querySelectorALl retorna um array com itens de acordo o passado como parâmetro
-
-
         if (botaoMarcarTodas.innerText === 'Marcar todos') {
-            const todosParagrafos = document.querySelectorAll('p');
-            console.log(todosParagrafos, "vai retornar todos os <p> encontrados");
+            const todosParagrafos = document.querySelectorAll('.elementoLista');
+            /*    console.log(todosParagrafos, "vai retornar todos os <p> encontrados"); */
 
             todosParagrafos.forEach(item => {
                 item.classList.add("checked");
             });
             botaoMarcarTodas.innerText = 'Desmarcar todos';
         } else {
-            const todosParagrafos = document.querySelectorAll('p');
+            const todosParagrafos = document.querySelectorAll('.elementoLista');
             todosParagrafos.forEach(item => {
                 item.classList.remove("checked");
             });
@@ -83,10 +66,14 @@ botao.addEventListener('click', (event) => {
 
     });
 
-    //TODO função de limpar
+    // função de limpar TUDO
     botaoLimpar.addEventListener('click', () => {
         //listaTarefas é <ul>
         listaTarefas.innerHTML = '';
     });
+
+ // Desafio extra
+ /* No html linhas 24 à 26 
+/* No javaScript >  linhas 26 e 32 */
 
 });
